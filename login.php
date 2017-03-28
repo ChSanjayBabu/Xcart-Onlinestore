e5b371f9ff6d6202dc9ee04295c832fb357ce81c
SanjayBabu-sopho <sanjay.ch222@gmail.com>
2017-03-28 12:03:49 +0000

    modified login

--- login.php
+++ login.php
@@ -1,58 +1,59 @@
 <?php
-    // for using $_SESSION["error"]
     session_start();
     
+    
     // if user reached page via GET (as by clicking a link or via redirect)
-    if ($_SERVER["REQUEST_METHOD"] == "GET")
+    if($_SERVER["REQUEST_METHOD"] == "GET")
     {
         // to show login page
         require("login_form.php");
     }
 
     // else if user reached page via POST (as by submitting a form via POST)
-    else if ($_SERVER["REQUEST_METHOD"] == "POST")
+    else if($_SERVER["REQUEST_METHOD"] == "POST")
     {
+        $num = 0;
         // validate submission
-        if (empty($_POST["email"]))
+        if(empty($_POST["email"]))
         {
             //we can use $_SESSION["error"] to change to div in login_page so it displays error
-            $_SESSION["error"] = "You must provide your username.";
-            require("login_form.php");
+            echo "You must provide your email<br>";
+            $num++;
         }
-        else if (empty($_POST["password"]))
+        if(empty($_POST["password"]))
         {
-            $_SESSION["error"] ="You must provide your password";
-            require("login_form.php");
+            echo "You must provide your password";
+            $num++;
         }
-        else
+        if($num == 0)
         {
+
             require("connect.php");
             // query database for user
-            $rows = mysqli_query($conn,"SELECT * FROM project2 WHERE email = '{$_POST["email"]}'");
-    
+            $result = mysqli_query($conn,"SELECT * FROM details WHERE email = '".$_POST["email"]."'");
+            
+            $Row = mysqli_fetch_array($result);
             // if we found user, check password
-            if (count($rows) == 1)
+            if($Row != NULL)
             {
-                // first (and only) row
-                $row = $rows[0];
-    
                 // compare hash of user's input against hash that's in database
-                if ($_POST["password"] == $row["pass"])
+                if ($_POST["password"] == $Row["pass"])
                 {
                     // remember that user's now logged in by storing user's ID in session
-                    $_SESSION["email"] = $row["email"];
-    
-                    // redirect to portfolio
-                    header("Location: portfolio.php");
+                    $_SESSION["email"] = $Row["email"];
+                    header('Location: portfolio.php');
                     exit;
+                }
+                else
+                {
+                    echo "wrong password<br>";
                 }
             }
     
             // else apologize
             else
             {
-                $_SESSION["error"] = "Invalid username and/or password.";
-                require("login_form.php");
+                echo "email doesn't exist<br>";
             }
         }
     }
