af1d208ec406a53d5741e57759af0acc91fa650f
Parth Sharma <parthsharma242@gmail.com>
2017-04-08 02:46:38 +0000

    index page was done

--- public_html/index.php
+++ public_html/index.php
@@ -1,26 +1,36 @@
 <!DOCTYPE html>
-<html>
-    <head>
-        <title>
-            index
-        </title>
-        <style>
-            body  
-            {
-                background-image: url("images/background-pattern.jpg");
-            }
-        </style>
-    </head>
-    <body>
-        <h4>
-            <?php
-                if(isset($msg))
-                {
-                    echo $msg;
-                }
-            ?>
-        </h4>
-        <a href="login_form.php">click here to sign in</a><br>
-        <a href="../models/store.php">Go to store</a><br>
-    </body>
-</html>
+	<html>
+		<head>
+			<title>cbayx</title>
+			<link href="css/index.css" rel="stylesheet" type="text/css"/>
+		</head> 
+		<body>
+			<div class="header-wrapper">
+				<div class="header-icon"><img src="images/Shopping-Cart.jpg" style="width:70px; height:60px;"/></div>
+				<div class="text">Welcome to Xcart (... a smart startup)</div>
+				<div class="logsign">
+					<b><a class="header-login" href="/login.html">Log In</a></b>
+					<i><span class="text-gray">or</span></i>
+					<b><a class="header-signup" href="/register.html">Visit Store</a></b>
+				</div>
+			</div>
+			<div class="msg">
+						 <h2>
+							 <?php
+			                if(isset($msg))
+			                {
+			                    echo $msg;
+			                }
+		                ?>
+	                </h2>
+						<div>
+							<h1 class="head">Built for Students</h1>
+							<p class="para">An ecommerce platform aiming at introducing college students to the world of ecommerce. Put things up for bidding, buy stuff from other students. As simple as that !</p>
+						</div>
+						<div class="decor">
+                         ==========================================================================================================
+						</div>
+			</div>
+		</body>
+	</html>
+	
